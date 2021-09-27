158794b6ca846d533b683918e377792e5ec2eaa4
Teddy Omondi <71281162+TeddyO328@users.noreply.github.com>
2021-09-27 22:06:31 +0300

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -11,5 +11,4 @@ def safe_print_list(my_list=[], x=0):
                 return index
         except IndexError:
             print()
-            return index
-            
+            return index     
